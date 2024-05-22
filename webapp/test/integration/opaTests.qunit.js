13f0d357c97eade15a04c1f47a8a3041b74d0315
fiori-logaligroup <rogerorsa@gmail.com>
2024-05-22 11:02:22 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["roger/invoicesmaster/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"roger/invoices_master/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
